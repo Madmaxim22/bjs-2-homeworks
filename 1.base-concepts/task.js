2b83309d0ed79d2648ed380c4155dd68bfcb2a1d
Максим Мухомедьяров <madmaxim22@yandex.ru>
2025-03-15 11:17:08 +0300

    implement solveEquation

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,14 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  if(discriminant == 0) {
+    arr[0] = (- b / (2 * a));
+  }
+  if(discriminant > 0) {
+    arr[0] = (- b + Math.sqrt(discriminant)) / (2 * a);
+    arr[1] = (- b - Math.sqrt(discriminant)) / (2 * a);
+  }
   return arr;
 }
 
